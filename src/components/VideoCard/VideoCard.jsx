30d9c098ac866fa024bf93a4e31c7be3d61266e5
Boierul <49337867+Boierul@users.noreply.github.com>
2022-09-02 10:33:32 +0200

    Implemented cards with video to siplay all the videos in the selected category.

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -9,8 +9,26 @@ import {demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChan
 export default function VideoCard({video: {id: {videoId}, snippet}}) {
     console.log(videoId)
     return (
-        <div>
-            VideoCard
-        </div>
+        <Card className={"card"}
+              sx={{width: {xs: '100%', sm: '358px', md: "320px",}, boxShadow: "none", borderRadius: 0}}>
+            <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY`}>
+                <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title}
+                           sx={{width: {xs: '100%', sm: '358px'}, height: 180}}
+                />
+            </Link>
+            <CardContent className={"card-content"} sx={{backgroundColor: "#202020", height: '56px'}}>
+                <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                    <Typography className={"card-title"} variant="subtitle1" fontWeight="bold" color="#FFF">
+                        {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+                    </Typography>
+                </Link>
+                <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+                    <Typography variant="subtitle2" color="gray">
+                        {snippet?.channelTitle || demoChannelTitle}
+                        <CheckCircleIcon sx={{fontSize: "12px", color: "gray", ml: "5px"}}/>
+                    </Typography>
+                </Link>
+            </CardContent>
+        </Card>
     )
 }
